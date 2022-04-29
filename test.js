51e576979bba563960d70c7f59340fe48eafd952
jpcapdevila <juan@capdevila.me>
2022-04-28 19:14:00 -0600

    Bump version

--- test.js
+++
@@ -1,6 +0,0 @@
-import {DatabaseMap} from "./index.js";
-const notes = new DatabaseMap('notes');
-tests.x.date.subscribe((value)=> {
-    console.log('updated value', value);
-});
-setInterval(async () => notes['dsa67d5sa7da'].date = new Date(), 1000);
